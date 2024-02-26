ff0492bfd6a33b3c7442c0c11036695f5e760952
IzanamiiDevv <rafaeloli596@gmail.com>
2024-02-27 01:19:48 +0800

    new files

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const crpto = require('crypto');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+const publicPath = path.join(__dirname,'public')
+
+app.use(express.static(publicPath));
+
+//middleware
+app.use(express.json());
+app.use(cors({
+    origin: "*"
+}));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(publicPath,'index.html'));
+});
+
+//Server Op
+
+
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`)
+});
